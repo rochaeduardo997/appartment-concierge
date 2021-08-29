66d2f21993605e7eda3d8ea0b979faed254c9070
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-08-28 21:31:29 -0300

    Created way to use two cores from CPU and rise a new worker in case of any process was kill

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,12 @@
 /* general imports */
-const express = require("express");
-const cors    = require("cors");
-const app     = express();
+const { cpus } = require("os");
+const { pid }  = require("process");
+const cluster  = require("cluster");
+const express  = require("express");
+const cors     = require("cors");
+
+/* starting services */
+const app = express();
 
 /* database startup */
 require("./database/database");
@@ -17,12 +22,23 @@ app.use(express.urlencoded({ limit: "50MB", extended: true }));
 /* routs */
 app.use(routes);
 
-/* defining server port and server startup*/
+/* defining server port and server startup */
 const SERVER_POST = 3000;
-app.listen(SERVER_POST, (err) => {
-  if(err) {
-    console.trace("Fail on startup server");
-  }
 
-  console.log(`Server running on http://localhost:${SERVER_POST}`);
-});
+if(cluster.isMaster){
+  cpus[0] = cluster.fork();
+  cpus[1] = cluster.fork();
+
+  cluster.on("exit", (err) => {
+    cluster.fork();
+    console.log(`New worker rising`);
+  });
+} else {
+  app.listen(SERVER_POST, (err) => {
+    if(err) {
+      console.trace("Fail on startup server");
+    }
+  
+    console.log(`PID:${pid} - Server running on http://localhost:${SERVER_POST}`);
+  });
+}
